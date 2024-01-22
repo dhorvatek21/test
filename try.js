5657fe90052687f2bff09f3a3ce68569a0b1e195
dhorvatek21 <domagoj.horvatek2@gmail.com>
2024-01-22 22:37:13 +0100

    new changes on try.js

--- try.js
+++ try.js
@@ -1,1 +1,6 @@
-import 
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateRandomArray = (length, minValue, maxValue) => Array.from({ length }, () => getRandomNumber(minValue, maxValue));
+
+const randomArray = generateRandomArray(5, 1, 10);
+console.log(randomArray);
